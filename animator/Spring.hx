1cdc83470b90d06e430f941b8dc828103857a26e
George Corney <haxiomic@gmail.com>
2020-08-17 18:49:04 +0100

    End spring when energy below a certain threshold

--- animator/Spring.hx
+++ animator/Spring.hx
@@ -10,6 +10,7 @@ class Spring {
 	public var strength: Float;
 	public var damping: Float;
 	public var onUpdate: Null<(value: Float, velocity: Float) -> Void>;
+	public var minEnergyThreshold = 1e-5;
 	
 	public function new(
 		initialValue: Float,
@@ -44,56 +45,65 @@ class Spring {
 		var k = this.strength;
 		var b = this.damping; // β in wolfram reference
 
-		var critical = k * 4 - b * b;
+		var totalEnergy =
+			0.5 * V0 * V0 +    // kinetic energy
+			0.5 * k * X0 * X0; // potential energy
 
-		if (critical > 0) {
-			// under damped
-			var q = 0.5 * sqrt(critical); // γ
-
-			var A = X0;
-			var B = ((b * X0) * 0.5 + V0) / q;
-
-			var m = exp(-b * 0.5 * t);
-			var c = cos(q * t);
-			var s = sin(q * t);
-
-			var x1 = m * (A*c + B*s);
-			var v1 = m * (
-				( B*q - 0.5*A*b) * c +
-				(-A*q - 0.5*b*B) * s
-			);
-
-			this.velocity = v1;
-			this.value = x1 + this.target;
-		} else if (critical < 0) {
-			// over damped
-			var u = 0.5 * sqrt(-critical);
-			var p = -0.5 * b + u;
-			var n = -0.5 * b - u;
-			var B = -(n*X0 - V0)/(2*u);
-			var A = X0 - B;
-
-			var ep = exp(p * t);
-			var en = exp(n * t);
-
-			var x1 = A * en + B * ep;
-			var v1 = A * n * en + B * p * ep;
-
-			this.velocity = v1;
-			this.value = x1 + this.target;
+		if (totalEnergy < minEnergyThreshold) {
+			this.velocity = 0;
+			this.value = this.target;
 		} else {
-			// critically damped
-			var w = sqrt(k); // ω
+			var critical = k * 4 - b * b;
 
-			var A = X0;
-			var B = V0 + w * X0;
-			var e = exp(-w * t);
+			if (critical > 0) {
+				// under damped
+				var q = 0.5 * sqrt(critical); // γ
 
-			var x1 = (A + B * t) * e;
-			var v1 = (B - w * (A + B * t)) * e;
+				var A = X0;
+				var B = ((b * X0) * 0.5 + V0) / q;
 
-			this.velocity = v1;
-			this.value = x1 + this.target;
+				var m = exp(-b * 0.5 * t);
+				var c = cos(q * t);
+				var s = sin(q * t);
+
+				var x1 = m * (A*c + B*s);
+				var v1 = m * (
+					( B*q - 0.5*A*b) * c +
+					(-A*q - 0.5*b*B) * s
+				);
+
+				this.velocity = v1;
+				this.value = x1 + this.target;
+			} else if (critical < 0) {
+				// over damped
+				var u = 0.5 * sqrt(-critical);
+				var p = -0.5 * b + u;
+				var n = -0.5 * b - u;
+				var B = -(n*X0 - V0)/(2*u);
+				var A = X0 - B;
+
+				var ep = exp(p * t);
+				var en = exp(n * t);
+
+				var x1 = A * en + B * ep;
+				var v1 = A * n * en + B * p * ep;
+
+				this.velocity = v1;
+				this.value = x1 + this.target;
+			} else {
+				// critically damped
+				var w = sqrt(k); // ω
+
+				var A = X0;
+				var B = V0 + w * X0;
+				var e = exp(-w * t);
+
+				var x1 = (A + B * t) * e;
+				var v1 = (B - w * (A + B * t)) * e;
+
+				this.velocity = v1;
+				this.value = x1 + this.target;
+			}
 		}
 
 		if (onUpdate != null) onUpdate(value, velocity);
