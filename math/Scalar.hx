a8ff663bcecb6b8d5c26d1e18cf7cc9c8895be98
George Corney <haxiomic@gmail.com>
2020-08-20 03:23:49 +0100

    New scalar fns

--- math/Scalar.hx
+++ math/Scalar.hx
@@ -14,4 +14,17 @@ class Scalar {
 		return Math.abs(x % 1);
 	}
 
+	static public inline function lerp(a: Float, b: Float, t: Float) {
+		return a * (1.0  - t) + b * t;
+	}
+
+	static public inline function clamp(x: Float, min: Float, max: Float) {
+		return x < min ? min : (x > max ? max : x);
+	}
+
+	static public inline function smoothstep(edge0: Float, edge1: Float, x: Float) {
+		var t = clamp((x - edge0) / (edge1 - edge0), 0.0, 1.0);
+		return t * t * (3.0 - 2.0 * t);
+	}
+
 }
