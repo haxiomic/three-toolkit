0925732078ae9bac188c52330e91db612be65ea6
George Corney <haxiomic@gmail.com>
2021-08-15 18:44:37 +0100

    Add new TopLevel class with utils to make available everywhere, starting with macro based extend()

---
+++ TopLevel.hx
@@ -0,0 +1,86 @@
+import haxe.macro.Context;
+import haxe.macro.Expr;
+
+/**
+	Extend a structure with another
+
+	If the extending structure has `@:optional` fields, these are skipped if `null`
+**/
+macro function extend(a, b) {
+	var aType = Context.followWithAbstracts(Context.typeof(a));
+	var bType = Context.followWithAbstracts(Context.typeof(b));
+
+	return switch [aType, bType] {
+		case [TAnonymous(_.get() => aAnon), TAnonymous(_.get() => bAnon)]:
+
+			var extendedFieldMap = new Map<String, Expr>();
+
+			for (field in aAnon.fields) {
+				var name = field.name;
+				var isOptional = field.meta.has(':optional');
+
+				extendedFieldMap.set(name, macro a.$name);
+			}
+
+			for (field in bAnon.fields) {
+				var name = field.name;
+				var isOptional = field.meta.has(':optional');
+
+				var expr = if (isOptional) {
+
+					// this block is just to improve on nullSafety which currently incorrectly infers Null<T>
+					var baseField = Lambda.find(aAnon.fields, cf -> cf.name == name);
+					if (baseField != null) {
+						var e = macro b.$name != null ? b.$name : a.$name;
+
+						var baseFieldIsOptional = baseField.meta.has(':optional');
+						if (!baseFieldIsOptional) {
+							var nonNullType = unwrapNull(Context.toComplexType(field.type));
+							e = macro ($e : $nonNullType);
+						}
+						e;
+					} else {
+						macro b.$name;
+					}
+				} else {
+					macro b.$name;
+				}
+
+				extendedFieldMap.set(name, expr);
+			}
+
+			var extendedFields: Array<ObjectField> = [
+				for (name => expr in extendedFieldMap) { field: name, expr: expr }
+			];
+
+			var objExpr: Expr = {
+				expr: EObjectDecl(extendedFields),
+				pos: Context.currentPos()
+			};
+
+			return macro {
+				var a = $a;
+				var b = $b;
+				$objExpr;
+			};
+		default:
+			Context.fatalError('Can only extend structures', Context.currentPos());
+	}
+}
+
+/**
+	Recursively unwraps Null<T> to T
+**/
+private function unwrapNull(complexType: ComplexType) {
+	return switch complexType {
+		case TPath({
+			pack: [],
+			name: 'StdTypes',
+			sub: 'Null',
+			params: [TPType(typeParam)],
+		}):
+			unwrapNull(typeParam);
+		default: 
+			complexType;
+	}
+}
