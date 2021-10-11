c9a1275c726b67c95a5046e7b19e9c41048d0a76
George Corney <haxiomic@gmail.com>
2021-10-11 12:42:53 +0100

    Doc

--- three/Uniform.hx
+++ three/Uniform.hx
@@ -1,5 +1,8 @@
 package three;
 
+/**
+	Overrides Uniform to give it a type parameter, which was removed from @types/three at one point
+**/
 @:jsRequire("three", "Uniform") extern class Uniform<T> {
 	@:overload(function(type:String, value:T): Uniform<T> { })
 	function new(value:T);
@@ -7,7 +10,7 @@ package three;
 	var value : T;
 	@:native("dynamic")
 	var dynamic_ : Bool;
-	var onUpdateCallback : haxe.Constraints.Function;
+	dynamic function onUpdateCallback():Void;
 	function onUpdate(callback:haxe.Constraints.Function):Uniform<T>;
 	static var prototype : Uniform<Any>;
 }
